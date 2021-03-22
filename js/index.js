6ac3bd50a779ef3d5baf9278842b7c5ee4f960ff
Krasilka <mad_gajka@mail.ru>
2021-03-22 17:22:19 +0000

    added clock

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// Clock 
+setInterval(updateClock, 1000);
+
+function updateClock() {
+    const clockContainer = document.querySelector('.clock');
+    clockContainer.innerText = new Date().toLocaleTimeString();
+}
